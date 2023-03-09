bd4769678cb9e3c4f3f8ae5ade4fbb2c2a088ed1
shrutik Nandeshwar <123307004+Shrutik1711@users.noreply.github.com>
2023-03-09 17:37:47 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first integer: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter the second integer: ");
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+    }
+}
